7fdd2a3a960ea35c0ffca4225e2fc58aa41786e0
sadiqsalau <sadiqsalau@users.noreply.github.com>
2024-09-19 13:20:39 +0000

    deploy: eleventeen-labs/distrxct-frontend-dev@57d9324b8468665bfeb45439f623712b20fbfe02

--- _next/static/chunks/app/friends/page-5488e8435d9ace97.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[89],{9009:function(n,e,u){Promise.resolve().then(u.t.bind(u,2559,23))}},function(n){n.O(0,[559,47,623,744],function(){return n(n.s=9009)}),_N_E=n.O()}]);
